1f2b76a09281022011e90faecbaa5ddc4714c9da
vittoriodedi <151766757+vittoriodedi@users.noreply.github.com>
2025-03-13 11:28:07 +0100

    Quarto Commit
    Quarto Commit

--- files/src/Controller/AboutController.php
+++ files/src/Controller/AboutController.php
@@ -4,18 +4,21 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class AboutController
+class AboutController extends AbstractController
 {
     #[Route('/about/{subject}', name: 'about')]
     public function index(?string $subject = null): Response
     {
-        if (null !== $subject) {
-            return new Response("<html><body> Ciao $subject! </body></html>");
-        }
+        return $this->render(
+            'about/index.html.twig',
+            [
+                'subject' => $subject,
+            ]
 
-        return new Response('<html><body> Seconda </body></html>');
+        );
     }
 }
--- files/src/Controller/HomePageController.php
+++ files/src/Controller/HomePageController.php
@@ -4,15 +4,16 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomePageController
+class HomePageController extends AbstractController
 {
     #[Route('/', name: 'home_page')]
     public function index(Request $request): Response
     {
-        return new Response('<html><body> Prima </body></html>');
+        return $this->render("home_page/index.html.twig");
     }
 }
