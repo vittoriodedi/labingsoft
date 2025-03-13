a2e1f16ce4450f1ecae206ea251381c68b75320f
vittoriodedi <151766757+vittoriodedi@users.noreply.github.com>
2025-03-13 09:45:39 +0100

    Secondo Commit
    Secondo Commit

---
+++ files/src/Controller/HomePageController.php
@@ -0,0 +1,17 @@
+<?php
+declare (strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomePageController
+{
+    #[Route('/')]
+    public function index(Request $request): Response
+    {
+        return new Response('<html><body> oiiaiiooiiiai!</body></html>');
+    }
+}
