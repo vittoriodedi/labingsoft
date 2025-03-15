749204a2cf26e98ca458e74daa9726e1878d797d
vittoriodedi <151766757+vittoriodedi@users.noreply.github.com>
2025-03-15 12:16:55 +0100

    Pre-Esercitazione

--- files/assets/app.js
+++ files/assets/app.js
@@ -4,6 +4,5 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import 'bootstrap';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
